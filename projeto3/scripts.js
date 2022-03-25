10932ada0966368ff97c6656a6d72559a642f83a
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2022-03-25 16:46:19 -0300

    Final da aula 29. Criando arquivo de script ====>> na seçao: 5. Introduçao ao JavaScript

---
+++ projeto3/scripts.js
@@ -0,0 +1,17 @@
+/**
+ * *****
+ * File: scripts.js
+ * Project: Curso de desenvolvimento de jogos web com Javascript
+ * Path: /home/desenvolvedor/cursos/curso-jogos-web-javascript/projeto3
+ * File Created: Friday, 25 March 2022 16:43:27
+ * Author: Marcos Antônio Barbosa de Souza (marcantech@uol.com.br)
+ * -----
+ * Last Modified: Friday, 25 March 2022 16:43:30
+ * Modified By: Marcos Antônio Barbosa de Souza (<marcantech@uol.com.br>)
+ * -----
+ * Copyright (c) 2022 All rights reserved, Marcant Tecnologia da Informação
+ * -----
+ * Description:
+ *  _____________ 29. Criando arquivo de script
+ * *****
+ */
