10be8eb3a5d4e7f5014fb2fc87363ac91449bce5
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2022-03-25 17:14:00 -0300

    Final da aula Aula 30. Variáveis ====>> na seçao: 5. Introduçao ao JavaScript

--- projeto3/scripts.js
+++ projeto3/scripts.js
@@ -6,12 +6,20 @@
  * File Created: Friday, 25 March 2022 16:43:27
  * Author: Marcos Antônio Barbosa de Souza (marcantech@uol.com.br)
  * -----
- * Last Modified: Friday, 25 March 2022 16:43:30
+ * Last Modified: Friday, 25 March 2022 17:13:18
  * Modified By: Marcos Antônio Barbosa de Souza (<marcantech@uol.com.br>)
  * -----
  * Copyright (c) 2022 All rights reserved, Marcant Tecnologia da Informação
  * -----
  * Description:
- *  _____________ 29. Criando arquivo de script
+ * ············ Aula 30. Variáveis
  * *****
  */
+
+let vida = 100;
+let forca = 10;
+let defesa = 5;
+
+let nome = 'Herói';
+
+let pulo = false;
