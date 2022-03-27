8b0a8a734585c9fd9033c525d81853d7f7ce0e87
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2022-03-27 13:06:03 -0300

    Final da aula 54. Trabalhando com textos ====>> na seçao: 6. Criando nosso primeiro jogo - Futebol Pong

--- pong-game/script.js
+++ pong-game/script.js
@@ -12,7 +12,7 @@
  * Copyright (c) 2022 All rights reserved, Marcant Tecnologia da Informação
  * -----
  * Description:
- * ············ Aula 53. Desenhando o jogador 2 e a bolinha
+ * ············ Aula 54. Trabalhando com textos
  * *****
  */
 let canvas = document.getElementById('canvas'); // integração com o HTML5
@@ -21,3 +21,9 @@ container.fillStyle = '#8b8b8b';
 let jogador1 = container.fillRect(20, 90, 15, 100); // <canvas id="canvas" width="430" height="280"></canvas>
 let jogador2 = container.fillRect(400, 90, 15, 100); // <canvas id="canvas" width="430" height="280"></canvas>
 let bolinha = container.fillRect(430 / 2 - 7.5, 280 / 2 - 7.5, 15, 15);
+
+container.font = '20px Arial';
+let pts1 = 0;
+let pts2 = 0;
+let score1 = container.fillText('Score 1: ' + pts1, 45, 50);
+let score2 = container.fillText('Score 2: ' + pts2, 290, 50);
