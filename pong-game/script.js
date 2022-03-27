0399bb525f66afd2081388839b46a9350299c150
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2022-03-27 09:55:20 -0300

    Final da aula 47. Criando projeto ====>> na seçao: 6. Criando nosso primeiro jogo - Pong

---
+++ pong-game/script.js
@@ -0,0 +1,17 @@
+/**
+ * *****
+ * File: script.js
+ * Project: Curso de desenvolvimento de jogos web com Javascript
+ * Path: /home/desenvolvedor/cursos/curso-jogos-web-javascript/pong-game
+ * File Created: Sunday, 27 March 2022 09:51:13
+ * Author: Marcos Antônio Barbosa de Souza (marcantech@uol.com.br)
+ * -----
+ * Last Modified: Sunday, 27 March 2022 09:51:15
+ * Modified By: Marcos Antônio Barbosa de Souza (<marcantech@uol.com.br>)
+ * -----
+ * Copyright (c) 2022 All rights reserved, Marcant Tecnologia da Informação
+ * -----
+ * Description:
+ * ············ Aula 47. Criando projeto
+ * *****
+ */
