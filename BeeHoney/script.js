cec780463a94fa107e55bc17667a55f3a65219b8
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2022-03-28 14:59:26 -0300

    Final da aula 72. Base para o projeto ====>> na seçao: 7. Introduçao a POO - BeeHoney

---
+++ BeeHoney/script.js
@@ -0,0 +1,17 @@
+/**
+ * *****
+ * File: script.js
+ * Project: Curso de desenvolvimento de jogos web com Javascript
+ * Path: /home/desenvolvedor/cursos/curso-jogos-web-javascript/BeeHoney
+ * File Created: Monday, 28 March 2022 14:50:19
+ * Author: Marcos Antônio Barbosa de Souza (marcantech@uol.com.br)
+ * -----
+ * Last Modified: Monday, 28 March 2022 14:50:21
+ * Modified By: Marcos Antônio Barbosa de Souza (<marcantech@uol.com.br>)
+ * -----
+ * Copyright (c) 2022 All rights reserved, Marcant Tecnologia da Informação
+ * -----
+ * Description:
+ * ············ Aula 72. Base para o projeto
+ * *****
+ */
