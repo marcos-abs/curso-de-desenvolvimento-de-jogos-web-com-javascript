c0a6ed8fc4092de605d12b71c7cb034688a79fa7
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2022-04-02 11:27:29 -0300

    Final da aula 99. Desenhando Obj na tela ====>> na seção 8 - Flappy Bird - Criando cenas

--- FlappyBird/script.js
+++ FlappyBird/script.js
@@ -12,8 +12,39 @@
  * Copyright (c) 2022 All rights reserved, Marcant Tecnologia da Informação
  * -----
  * Description:
- * ············ Aula 97. Base do projeto
+ * ············ Aula 99. Desenhando Obj na tela
  * *****
  */
 
 let canvas = document.getElementById('canvas').getContext('2d');
+const areaTela = {
+    ix: 0,
+    iy: 0,
+    tx: 430,
+    ty: 560,
+};
+let bg = new Obj(
+    areaTela.ix,
+    areaTela.iy,
+    areaTela.tx,
+    areaTela.ty,
+    'assets/images/sky.png',
+);
+
+function draw() {
+    bg.draw();
+}
+
+function update() {}
+
+function clearScreen() {
+    canvas.clearRect(areaTela.ix, areaTela.iy, areaTela.tx, areaTela.ty);
+}
+
+function main() {
+    clearScreen();
+    draw();
+    update();
+}
+
+let refreshCanvas = setInterval(main, 100);
--- FlappyBird/utils.js
+++ FlappyBird/utils.js
@@ -15,3 +15,18 @@
  * ············ Aula 97. Base do projeto
  * *****
  */
+class Obj {
+    constructor(x, y, width, height, image) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.image = image;
+    }
+
+    draw() {
+        let img = new Image();
+        img.src = this.image;
+        canvas.drawImage(img, this.x, this.y, this.width, this.height);
+    }
+}
