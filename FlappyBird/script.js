68583c6e6d6ab287bcc7823f851d5821ec38f470
Marcos Antônio Barbosa de Souza <desouza.marcos@uol.com.br>
2022-04-02 00:08:57 -0300

    Final da aula 97. Base do projeto (1m55s de 4m27s) ====>> na seção 8 - Flappy Bird - Criando cenas

---
+++ FlappyBird/script.js
@@ -0,0 +1,17 @@
+/**
+ * *****
+ * File: script.js
+ * Project: Curso de desenvolvimento de jogos web com Javascript
+ * Path: /home/desenvolvedor/cursos/curso-jogos-web-javascript/FlappyBird
+ * File Created: Saturday, 02 April 2022 00:03:11
+ * Author: Marcos Antônio Barbosa de Souza (marcantech@uol.com.br)
+ * -----
+ * Last Modified: Saturday, 02 April 2022 00:03:13
+ * Modified By: Marcos Antônio Barbosa de Souza (<marcantech@uol.com.br>)
+ * -----
+ * Copyright (c) 2022 All rights reserved, Marcant Tecnologia da Informação
+ * -----
+ * Description:
+ * ············ Aula 97. Base do projeto
+ * *****
+ */
